d6e086b11e7f80c3a7078fe289e8b3373fb6d714
Gursaanj <gursaanjresides@gmail.com>
2018-12-30 00:45:01 -0800

    Added Neccesary Files
    Added my first tryout fil;es as well as the package for the NavMeshBaker and NavMeshSurfaces

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Unity_3D_Navigation_Mesh_Tutorial
+
+
+In this project I use UNITY 3D to learn about and play around with Navigation Meshes.
+
+Things like baking, varying cost areas and different forms of navigation mesh agents and navigation mesh obstacles. As well as generating off mesh links (both automated and manual).
+
+I also toyed around with the addition Navigation mesh packages on Unity's Github. In which Navigation meshes and can be pre baked and baked in real time. allowing certain methods to be called so that new meshes and surface area costs can be made in real time.
+
+This repository will mainly be used to play around with different ideas and toy with the packages.
---
+++ unity pathfinding tutorial/Assets/Code/NPCConnectedPatrol.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCConnectedPatrol : MonoBehaviour {
+    //Does the agent wait on each node 
+    [SerializeField]
+    bool _patrolWaiting;
+
+    //the amount of time spent at each node
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    //the probability of wating at a certain node
+    [SerializeField]
+    float _waitProbability = 0.15f;
+
+    ////The probability of switching direction during patrol
+    //[SerializeField]
+    //float _switchProbability = 0.2F;
+
+    ////The list of all nodes to visit during patrol
+    //[SerializeField]
+    //List<Waypoint> _patrolPoints;
+
+    //Used Private variables
+    NavMeshAgent _navMeshAgent;
+    //int _currentPatrolIndex;
+    ConnectedWaypoint _currentWaypoint;
+    ConnectedWaypoint _previouseWaypoint;
+       
+    bool _travelling;
+    bool _waiting;
+    //bool _patrolForward;
+    float _waitTimer;
+    int _waypointsVisited;
+
+    // Use this for initialization
+    public void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent is not attached to" + gameObject.name);
+        }
+        else
+        {
+            if (_currentWaypoint == null)
+            {
+                //if there is no current way point set one at random
+                // grab all waypoints objects in the scene
+                GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+                if (allWaypoints.Length > 0)
+                {
+                    while (_currentWaypoint == null)
+                    {
+                        int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                        ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                        // when we end up finding a waypoint
+                        if (startingWaypoint != null)
+                        {
+                            _currentWaypoint = startingWaypoint;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log("Insufficient patrol points for basic patrolling behaviour");
+                }
+            }
+
+            SetDestination();
+
+        }
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // Are we close to the destination
+        if (_travelling && _navMeshAgent.remainingDistance <= 1.0F)
+        {
+            _travelling = false;
+            _waypointsVisited++;
+            SetWait();
+
+
+            //If we're going to wait, then wait
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                //ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //Instead if we're waiting
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if (_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+                _patrolWaiting = false;
+                _travelling = true;
+                SetDestination();
+            }
+        }
+    }
+
+    private void SetWait()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= _waitProbability)
+        {
+            _patrolWaiting = true;
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_waypointsVisited > 0)
+        {
+            //Vector3 targetVector = _patrolPoints[_currentPatrolIndex].transform.position;
+
+            // check for the next waypoint making sure it isn't the same as the previous waypoint
+            ConnectedWaypoint nextWaypoint = _currentWaypoint.NextWaypoint(_previouseWaypoint);
+            // make sure the current waypoint becomes the new waypoint
+            _previouseWaypoint = _currentWaypoint;
+            _currentWaypoint = nextWaypoint;
+        }
+
+        Vector3 targetVector = _currentWaypoint.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        _travelling = true;
+        }
+    }
+    // Doesn't need to be used
+    // Selects a new patrol point in the available list but also with a small probability allows for us to move forward or backwards
+    //private void ChangePatrolPoint()
+    //{
+    //    if (UnityEngine.Random.Range(0f, 1f) <= _switchProbability)
+    //    {
+    //        _patrolForward = !_patrolForward;
+    //    }
+
+    //    if (_patrolForward)
+    //    {
+    //        _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Count;
+    //    }
+    //    else
+    //    {
+    //        _currentPatrolIndex--;
+    //        if (_currentPatrolIndex < 0)
+    //        {
+    //            _currentPatrolIndex = _patrolPoints.Count - 1;
+    //        }
+    //    }
+    //}
